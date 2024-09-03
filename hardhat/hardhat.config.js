ca706fb31a5de695f5271f19920ad5cc56c7e197
Rajveer Singh Khanduja <rajveerkhanduja74@gmail.com>
2024-09-03 11:18:32 +0530

    Updated hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
     networks: {
         hardhat: {},
         sepolia: {
-            url: `https://sepolia.infura.io/v3/096eb421bab148a4bda4d72d6415d9ac`,
-            accounts: [`bc8b195941b9bef4c04779953b41d009bfcf7896e2e258ee8d77b4d0d3801ee5`]
-        }
-    }
+            url: process.env.SEPOLIA_URL_WITH_API_KEY,
+            accounts: [process.env.PRIVATE_KEY]
+        },
+    },
 };
